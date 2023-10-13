fc7eca908ac7343d2b90f6c05c74baef6c0bd483
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2023-10-12 22:32:49 -0600

    dí una solución a los distintos tamaños que tenían los cuadros de las obras agregué un cuadro de texto con hover

--- src/scripts/scriptBarraDeNavegacion.js
+++ src/scripts/scriptBarraDeNavegacion.js
@@ -1,5 +1,5 @@
 const botonAbrir= document.getElementById("botonAbrir");
-const botonCerrar= document.getElementById("botonCerrar");
+// const botonCerrar= document.getElementById("botonCerrar");
 const contenedorNavList= document.getElementById("contenedorNavList");
 const navList = document.getElementById("navList");
 let styleNavList= window.getComputedStyle(navList);// asi guaramos un objeto que tendrá los valores del CSS de contenedor navList
@@ -38,12 +38,12 @@ window.addEventListener("resize",()=>{
    /* console.log("el valor de flex direction es: " + direccionNavList);*/
    //Línea de codigo utilizada para confirmar la salida que arroja la función 
 if(innerWidth>770) {
-   botonCerrar.classList.remove("visible"); 
+//    botonCerrar.classList.remove("visible"); 
 }
 
 if(innerWidth<770 && direccionNavList =='column' && visibilityContenedorNavList=="visible" ) {
     
-    botonCerrar.classList.add("visible"); 
+    // botonCerrar.classList.add("visible"); 
  }
 
 });
